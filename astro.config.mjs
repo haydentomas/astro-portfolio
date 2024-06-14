4bfa11a07e2651ea92f42da4316d402320541b69
pixaful <goteamtomas@gmail.com>
2024-06-14 20:05:41 +0100

    Added basic SEO to add pages, single blogs still need doing

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://astro-blog-with-pagination-template.vercel.app',
+	site: 'https://www.haydentomas.co.uk',
 	integrations: [mdx(), sitemap()],
 });
