12dfef19200b77f379fa435062fa5e6f16db935a
pixaful <goteamtomas@gmail.com>
2024-06-14 20:38:38 +0100

    adding custom open graph images to project posts

--- src/content/config.ts
+++ src/content/config.ts
@@ -50,6 +50,7 @@ const postsCollection = defineCollection({
       tags: z.array(z.string()),
       projectURL: z.string(),
       pageURL: z.string(),
+      ogImage: z.string().optional(), // Make it optional in case a post doesn't have a custom OG image
       icons: z.array(z.enum(["javascript", "react", "html", /* Add more as needed */])),
       // Add a 'draft' property, which is a boolean false or true no ``
       draft: z.optional(z.boolean()),
