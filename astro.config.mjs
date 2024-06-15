a391b487271789e082339c61a4205d298dcb054b
pixaful <goteamtomas@gmail.com>
2024-06-15 17:20:32 +0100

    root url fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://www.haydentomas.co.uk',
+	site: 'https://astro-cv-portfolio.netlify.app',
 	integrations: [mdx(), sitemap()],
 });
