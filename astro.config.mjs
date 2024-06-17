cf56fe74a5622a3617cc70ae61b3acabffe5a281
pixaful <goteamtomas@gmail.com>
2024-06-17 21:29:12 +0100

    flowbite tests

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://astro-cv-portfolio.netlify.app',
-	integrations: [mdx(), sitemap()],
+  site: 'https://astro-cv-portfolio.netlify.app',
+  integrations: [mdx(), sitemap(), tailwind()]
 });
