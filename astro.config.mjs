b3a64e8f29e63413650f1af5f513e186b99fe72e
hayden Tomas <hello@haydentomas.com>
2024-07-03 13:47:43 +0100

    try again on build

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 import tailwind from "@astrojs/tailwind";
-
+import netlify from '@astrojs/netlify/functions';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://haydentomas.co.uk',
