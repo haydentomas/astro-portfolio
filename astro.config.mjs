e04f0ec658eb344d47fb6b8ecad79dafc635182c
hayden Tomas <hello@haydentomas.com>
2024-07-03 13:51:01 +0100

    more redirect checks

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import netlify from '@astrojs/netlify/functions';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://haydentomas.co.uk',
-  output: 'server',
   adapter: netlify(),
   trailingSlash: 'never',
   integrations: [mdx(), sitemap(), tailwind()]
