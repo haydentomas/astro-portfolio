bad3712047b93f80443e1a6fb8ad5de86e1c1ee1
hayden Tomas <hello@haydentomas.com>
2024-07-03 14:00:48 +0100

    undo some bits

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,8 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 import tailwind from "@astrojs/tailwind";
-import netlify from '@astrojs/netlify/functions';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://haydentomas.co.uk',
-  adapter: netlify(),
-  trailingSlash: 'never',
   integrations: [mdx(), sitemap(), tailwind()]
 });
