a12c2ed38a46b15978cec935ce147e95d41a31fd
hayden Tomas <hello@haydentomas.com>
2024-07-05 09:08:17 +0100

    attempting netflify forms once more

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://haydentomas.co.uk',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind()],
+  output: "server",
+  adapter: netlify()
 });
