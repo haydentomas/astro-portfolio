ad5f12d90239a09e3eba8e9d8ee1865fac88999c
hayden Tomas <hello@haydentomas.com>
2024-07-05 14:04:02 +0100

    broke blog and projects , this should fix it

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,9 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
-import netlify from "@astrojs/netlify";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://haydentomas.co.uk',
   integrations: [mdx(), sitemap(), tailwind()],
-  output: "server",
-  adapter: netlify()
+
 });
