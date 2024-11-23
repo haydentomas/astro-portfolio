fe17035a1d35f9a184929ef2a9ad597bb87de21f
Hayden Tomas <goteamtomas@gmail.com>
2024-11-23 13:26:24 +0000

    astro critters added

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,14 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
+import critters from 'astro-critters';
+
 // https://astro.build/config
 export default defineConfig({
   image: {
     serviceEntryPoint: '@astrojs/image/sharp',
   },
   site: 'https://haydentomas.co.uk',
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), critters()],
 
 });
